75869c3b09bae69fc9fe2ab326851622af06b21b
MartynLCD3 <martincastagno63@gmail.com>
2023-02-01 21:30:56 -0300

    conceptos básicos - print

---
+++ conceptos_basicos/print.py
@@ -0,0 +1,19 @@
+# @Forma de imprimir mensajes
+print("hello world")  # hello world
+
+print("###########")
+
+# @Operaciones simples
+print(2 + 2)  # 4
+print(6 * 5)  # 30
+print(4 * 12 - 2) # 46
+print(49 - 4 / 12)  # 48.6
+print(5 * (4 + 10)) # 70
+print(2 ** 4) # 16
+print(4 ** (1 / 2)) # 2.0
+print(20 // 6) # @cociente 3
+print(10 // 4) # @cociente 2
+print(20 % 6) # @resto 2
+print(10 % 4) # @resto 2
+
+# @recordar: en Python existen los tipos de datos string, int y float.
