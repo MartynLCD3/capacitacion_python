b8c705e4e9c6e9b0b3e6484e29a9c68c21513908
MartynLCD3 <martincastagno63@gmail.com>
2023-02-02 18:07:58 -0300

    conceptos básicos - cadenas

---
+++ 02_cadenas/cadenas.py
@@ -0,0 +1,19 @@
+# Se puede usar comillas simples o dobles
+
+print("comillas doble") # comillas doble
+print('comillas simple') # comillas simple
+
+# Barra invertida
+print('Barra\'s invertida ') # Barra's invertida
+
+# Salto de linea
+print('Salto \nde \nlinea')
+
+# Nuevo salto de linea
+print("""Esto es como
+un comentario multilinea""")
+
+# Operaciones: concatenación
+print("Conca" + "te" + "nación") # Concatenación
+print("spam" * 3) # spamspamspam
+print(4 * '2') # 2222
